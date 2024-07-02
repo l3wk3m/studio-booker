991079e88c9b310975baa2d65bb06904b67d15d4
Luke Maycock <maycock.luke@gmail.com>
2024-07-02 00:53:40 +0000

    test: attempting to fix deployment error

--- studiobooker/urls.py
+++ studiobooker/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from book.views import book
+from book.views import booker
 
 urlpatterns = [
-    path('book/', book, name='book'),
+    path('book/', booker, name='book'),
     path('admin/', admin.site.urls),
 ]
