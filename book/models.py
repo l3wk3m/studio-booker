c62d4a304427a844f3ee3928d13b9e327d3ed171
Luke Maycock <maycock.luke@gmail.com>
2024-07-04 16:01:07 +0000

    Adding custom model for user and model for studio db

--- book/models.py
+++ book/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = ((0, "Unavailable"), (1, "Available"))
 
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+# Artist model extending base user model
+class Artist(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20, null=True, blank=True)
+    last_name = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    contact_phone = models.CharField(max_length=15, null=True, blank=True)
+    
+    def __str__(self):
+            return f"{self.first_name + ' ' + self.last_name}"
+
+class Studios(models.Model):
+    """
+    Django database model for studio spaces
+    """
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 
