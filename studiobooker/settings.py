43cd842df24c7562c62c27495c31467ea62b7e0e
Luke Maycock <maycock.luke@gmail.com>
2024-07-04 14:51:31 +0000

    Updating files to make heroku compatible

--- studiobooker/settings.py
+++ studiobooker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8i8$_ndm%iy)*)3zp(q$rh1rm7tb*^+aa-e0rpwq*x$lwisb*a'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,6 +88,8 @@ WSGI_APPLICATION = 'studiobooker.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
