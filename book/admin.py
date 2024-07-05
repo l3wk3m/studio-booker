17318e0aeb4ccc021de63f714519a6d2554cc000
Luke Maycock <maycock.luke@gmail.com>
2024-07-05 10:21:51 +0000

    Installed summernote

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Artist, Studio, StudioBooking
 
 # Register your models here.
+admin.site.register(Artist)
+admin.site.register(Studio)
+admin.site.register(StudioBooking)
